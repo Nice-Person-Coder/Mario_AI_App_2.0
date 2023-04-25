6cd18dc1d046dcb59902204e746d0ebdcc8ba8ca
Nice-Person-Coder <110208689+Nice-Person-Coder@users.noreply.github.com>
2023-04-24 19:34:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+    video.size(800, 400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+function modelLoaded(){
+	  console.log("Model Loaded!");  
+	 }
+
+   function gotPoses(results){
+    if(results.length > 0 ){
+	    
+		console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+     }
+   }
+
+
+
+
+
+
+
+// img = "";
+// noseX = 0;
+// noseY = 0;
+// marioX = 325;
+// marioY = 325;
+
+// function preload()
+// {
+//  img = loadImage("mario05.png");
+// }
+
+// function setup() {
+//  createCanvas(650, 400);
+//  video = createCapture(VIDEO);
+//  video.size(600, 300);
+  
+//  poseNet = ml5.poseNet(video, modelLoaded);
+//  poseNet.on('pose', gotPoses);
+// }
+
+// function modelLoaded(){
+//   console.log("Model Loaded!");  
+// }
+
+// function gotPoses(results){
+//   if(results.length > 0 ){
+// noseX = results[0].pose.nose.x;
+// noseY = results[0].pose.nose.y;
+// console.log("noseX = " + noseX + "noseY = " + noseY);
+//   }
+// }
+
+// function draw() {
+//   background("#D3D3D3");
+//   if(noseX < 300){
+	// marioX = marioX - 2;
+//   }
+//   if(noseX > 300){
+// marioX = marioX + 2;
+//   }
+//   if(noseY < 150){
+// marioY = marioY - 2;
+//   }
+//   image(img,marioX, marioY, 40,70);
+// }
+
